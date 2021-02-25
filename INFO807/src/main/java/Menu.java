812cd3e0f649f256b02cf293efe28acbe27d8e27
bpassieux <bertrand.passieux@gmail.com>
2021-02-25 11:40:25 +0100

    creation-dieu

--- INFO807/src/main/java/Menu.java
+++ INFO807/src/main/java/Menu.java
@@ -18,6 +18,34 @@ public class Menu {
         Partie partie = new Partie();
         Plateau plateau = new Plateau();
         partie.plateau = plateau;
+        // God mod player
+        partie.ajouterJoueur("God");
+        Joueur god = partie.joueur.get(0);
+        // 2 quartiers de la première partie du plateau
+        /*partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(1));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(3));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(6));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(8));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(9));*/
+        ((Terrain)partie.plateau.getPlateau().get(1)).etatTerrain.achatPropriete(god);
+        ((Terrain)partie.plateau.getPlateau().get(3)).etatTerrain.achatPropriete(god);
+        ((Terrain)partie.plateau.getPlateau().get(6)).etatTerrain.achatPropriete(god);
+        ((Terrain)partie.plateau.getPlateau().get(8)).etatTerrain.achatPropriete(god);
+        ((Terrain)partie.plateau.getPlateau().get(9)).etatTerrain.achatPropriete(god);
+        // Les gares
+        /*partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(5));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(15));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(25));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(35));*/
+        ((Gare)partie.plateau.getPlateau().get(5)).etatGare.achatPropriete(god);
+        ((Gare)partie.plateau.getPlateau().get(15)).etatGare.achatPropriete(god);
+        ((Gare)partie.plateau.getPlateau().get(25)).etatGare.achatPropriete(god);
+        ((Gare)partie.plateau.getPlateau().get(35)).etatGare.achatPropriete(god);
+        // Les services
+        /*partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(12));
+        partie.joueur.get(0).propriete.add((Propriete) partie.plateau.getPlateau().get(28));*/
+        ((Service)partie.plateau.getPlateau().get(12)).etatService.achatPropriete(god);
+        ((Service)partie.plateau.getPlateau().get(28)).etatService.achatPropriete(god);
 
         while (!finiInit){
             System.out.println("Bienvenue dans le Monopoly de Al Rasheed Hamze, Passieux Bertrand et Wagner Robin");
